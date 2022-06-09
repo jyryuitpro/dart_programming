7c99e39dd630d21977b99957bd1c1db6f17a6a5f
jyr <jyr@wavers.co.kr>
2022-06-09 12:33:57 +0900

    Dart #1 기본기 - 변수 선언

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,5 +1,20 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_programming.calculate()}!');
+void main() {
+  // variable
+  var name = '코드팩토리';
+
+  print(name);
+
+  var name2 = '레드벨벳';
+
+  print(name2);
+
+  name = '플러터 프로그래밍';
+
+  print(name);
+
+  // Error: 'name' is already declared in this scope.
+  // var name = '코드팩토리2';
+  // print(name);
 }
