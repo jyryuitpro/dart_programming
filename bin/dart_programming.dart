c6b88b5588b30cbb673ad0ea3df867ce2c170494
jyr <jyr@wavers.co.kr>
2022-06-09 16:35:03 +0900

    Dart #1 기본기 - nullable, non-nullable

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,100 +1,59 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  // 정수
-  // integer
-  // int number1 = 10;
-
-  // print(number1);
-
-  // int number2 = 15;
-
-  // print(number2);
-
-  // int number3 = -20;
-
-  // int number1 = 2;
-  // int number2 = 4;
-
-  // print(number1 + number2);
-
-  // print(number1 - number2);
-
-  // print(number1 / number2);
-
-  // print(number1 * number2);
-
-  // 실수
-  // double
-  // double number1 = 2.5;
-  // double number2 =0.5;
-
-  // print(number1 + number2);
-
-  // print(number1 - number2);
-
-  // print(number1 / number2);
-
-  // print(number1 * number2);
-
-  // 맞다 / 틀리다
-  // Boolean
-  // bool isTrue = true;
-  // bool isFalse = false;
-
-  // print(isTrue);
-  // print(isFalse);
-
-  // 글자 타입
-  // String
-  // String name = '레드벨벳';
-  // String name2 = '코드팩토리';
+  // nullable - null이 될 수 있다.
+  // non-nullable - null이 될 수 없다.
+  // null - 아무런 값도 있지 않다.
+  // String name = '코드팩토리';
 
   // print(name);
+
+  // Error: The value 'null' can't be assigned to a variable of type 'String' because 'String' is not nullable.
+  // name = null;
+
+  // String? name2 = '블랙핑크';
+
+  // name2 = null;
+
   // print(name2);
 
-  // var(타입 추론) String
-  // var name3 = '블랙핑크';
-  // var number = 20;
+  // !: null이 아니다 즉, 현재 이 값은 null이 아니다.
+  // print(name2!);
 
-  // print(name3.runtimeType);
-
-  // Map<String, Map<int, List<double>>> testType = {};
-  // var testType = {};
-
-  // String name = '레드벨벳';
-  // String name2 = '슬기';
-
-  // print(name + name2);
-  // print(name + ' ' + name2);
-
-  // print('${name} ${name2}');
-
-  // print('$name $name2');
-
-  // print('${name.runtimeType} $name2');
-
-  // print('$name.runtimeType $name2');
-
-  dynamic name ='코드팩토리';
+  final String name = '코드팩토리';
 
   print(name);
 
-  dynamic number = 1;
+  // final로 선언하면 값을 변경할 수 없다.
+  // name = '블랙핑크';
 
-  print(number);
-
-  // 타입을 한번 선언하면
-  var name2 = '블랙핑크';
+  const String name2 = '블랙핑크';
 
   print(name2);
 
-  print(name.runtimeType);
-  print(name2.runtimeType);
+  // final과 마찬가지로 const로 선언한 변수는 값을 변경할 수 없다.
+  // name2 = '코드팩토리';
 
-  name = 2;
+  // final과 const를 사용하면 var 키워드를 생략할 수 있다. 따라서 타입을 생략하는 효과를 가지고 있다.
 
-  // var 한번 선언하면 선언할 때의 그 타입으로 fix가 되어 다른 타입으로 중간에 변경이 불가능
-  // Error: A value of type 'int' can't be assigned to a variable of type 'String'.
-  // name2 = 5;
+  final name3 = '코드팩토리';
+
+  print(name3);
+
+  const name4 = '블랙핑크';
+
+  print(name4);
+
+  // final과 const의 차이점
+  // 해당 코드가 실행이 되는 순간, 해당값을 변수에 저장한다.
+  // 빌드타임에 해당 값을 알고 있지 않아도 된다. (런타임)
+  final DateTime now = DateTime.now();
+
+  print(now);
+
+  // const: 빌드타임에 해당 값을 알고 있어야한다. (작성하는 순간에 값을 알고 있어야 한다.)
+  // Error: Cannot invoke a non-'const' constructor where a const expression is expected.
+  // const DateTime now2 = DateTime.now();
+
+  // print(now2);
 }
