31eee960206177649d57ad19579beb1caefe9572
jyr <jyr@wavers.co.kr>
2022-06-10 09:46:40 +0900

    Dart #1 기본기 - switch

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,23 +1,21 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  // if 문
+  // switch 문
 
-  // int number = 2;
-  // int number = 3;
-  int number = 4;
+  int number = 3;
 
-  if (number % 2 == 0) {
-    print('값이 짝수입니다.');
-  } else {
-    print('값이 홀수입니다');
-  }
+  switch (number % 3) {
+    case 0:
+      print('나머지가 0입니다.');
+      break;
 
-  if (number % 3 == 0) {
-    print('나머지가 0입니다.');
-  } else if (number % 3 == 1) {
-    print('나머지가 1입니다.');
-  } else {
-    print('나머지가 2입니다.');
+    case 1:
+      print('나머지가 1입니다.');
+      break;
+
+    default:
+      print('나머지가 2입니다.');
+      break;
   }
 }
