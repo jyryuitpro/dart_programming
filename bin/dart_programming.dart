6dda2a6843ebe938e2bc1e83ba6d3586e0e3b3e6
jyr <jyr@wavers.co.kr>
2022-06-10 09:38:49 +0900

    Dart #1 기본기 - Set

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,48 +1,23 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  // Map
-  // Key / Value
-  Map<String, String> dictionary = {
-    'Harry Potter': '해리포터',
-    'Ron Weasely': '본 위즐리',
-    'Hermione Granger': '헤르미온느 그레인저',
+  // Set: 중복을 자동으로 처리해준다.
+  final Set<String> names = {
+    'Code Factory',
+    'Flutter',
+    'Black Pink',
+    'Flutter',
   };
 
-  print(dictionary);
+  print(names);
 
-  Map<String, bool> isHarryPotter = {
-    'Harry Potter': true,
-    'Ron Weasely': true,
-    'Ironman': false,
-  };
+  names.add('Jenny');
 
-  print(isHarryPotter);
+  print(names);
 
-  isHarryPotter.addAll({
-    'Spiderman': false,
-  });
-  
-  print(isHarryPotter);
+  names.remove('Jenny');
 
-  print(isHarryPotter['Ironman']);
+  print(names);
 
-  isHarryPotter['Hulk'] = false;
-
-  print(isHarryPotter);
-
-  isHarryPotter['Spiderman'] = true;
-
-  print(isHarryPotter);
-
-  print('-------------------------');
-
-  print(isHarryPotter);
-
-  isHarryPotter.remove('Harry Potter');
-
-  print(isHarryPotter);
-
-  print(isHarryPotter.keys);
-  print(isHarryPotter.values);
+  print(names.contains('Flutter'));
 }
