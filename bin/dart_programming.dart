d4a70e7eaa754beb819a29c06385fe1b20817ee4
jyr <jyr@wavers.co.kr>
2022-06-10 09:35:18 +0900

    Dart #1 기본기 - Map

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,34 +1,48 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  // List
-  // 리스트
-  List<String> blackPink = ['제니', '지수', '로제', '리사'];
-  List<int> numbers = [1, 2, 3, 4, 5, 6];
+  // Map
+  // Key / Value
+  Map<String, String> dictionary = {
+    'Harry Potter': '해리포터',
+    'Ron Weasely': '본 위즐리',
+    'Hermione Granger': '헤르미온느 그레인저',
+  };
 
-  print(blackPink);
-  print(numbers);
+  print(dictionary);
 
-  // index
-  // 순서
-  // 0 부터시작
-  print(blackPink[0]);
-  print(blackPink[1]);
-  print(blackPink[2]);
-  print(blackPink[3]);
-  // Invalid value: Not in inclusive range 0..3: 4
-  // print(blackPink[4]);
+  Map<String, bool> isHarryPotter = {
+    'Harry Potter': true,
+    'Ron Weasely': true,
+    'Ironman': false,
+  };
 
-  print(blackPink.length);
+  print(isHarryPotter);
 
-  blackPink.add('코드팩토리');
+  isHarryPotter.addAll({
+    'Spiderman': false,
+  });
+  
+  print(isHarryPotter);
 
-  print(blackPink);
+  print(isHarryPotter['Ironman']);
 
-  blackPink.remove('코드팩토리');
+  isHarryPotter['Hulk'] = false;
 
-  print(blackPink);
+  print(isHarryPotter);
 
-  print(blackPink.indexOf('로제'));
+  isHarryPotter['Spiderman'] = true;
 
+  print(isHarryPotter);
+
+  print('-------------------------');
+
+  print(isHarryPotter);
+
+  isHarryPotter.remove('Harry Potter');
+
+  print(isHarryPotter);
+
+  print(isHarryPotter.keys);
+  print(isHarryPotter.values);
 }
