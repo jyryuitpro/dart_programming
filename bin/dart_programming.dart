2b513e878868d53ef1a93fd938525f1f09748c0c
jyr <jyr@wavers.co.kr>
2022-06-11 19:09:02 +0900

    Dart #4 비동기 프로그래밍 (Async Programming) - 1

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,70 +1,14 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  List<Map<String, String>> people = [
-    {
-      'name': '로제',
-      'group': '블랙핑크',
-    },
-    {
-      'name': '지수',
-      'group': '블랙핑크',
-    },
-    {
-      'name': 'RM',
-      'group': 'BTS',
-    },
-    {
-      'name': '뷔',
-      'group': 'BTS',
-    },
-  ];
-
-  print(people);
-
-  final parsedPeople = people
-      .map(
-        (x) => Person(
-          name: x['name']!,
-          group: x['group']!,
-        ),
-      )
-      .toList();
-
-  print(parsedPeople);
-
-  for (Person person in parsedPeople) {
-    print(person.name);
-    print(person.group);
-  }
-
-  final bts = parsedPeople.where(
-    (x) => x.group == 'BTS',
-  );
-
-  print(bts);
-
-  final result = people.map(
-        (x) => Person(
-      name: x['name']!,
-      group: x['group']!,
-    ),
-  ).where((x) => x.group == 'BTS').fold<int>(0, (prev, next) => prev + next.name.length);
-
-  print(result);
+  addNumbers(1, 1);
+  addNumbers(2, 2);
 }
 
-class Person {
-  final String name;
-  final String group;
+// 1 + 1 = 2
+// 2 + 2 = 4
+void addNumbers(int number1, int number2) {
+  print('계산중: $number1 + $number2');
 
-  Person({
-    required this.name,
-    required this.group,
-  });
-
-  @override
-  String toString() {
-    return 'Person(name:$name, group:$group)';
-  }
+  print('계산 완료: ${number1 + number2}');
 }
