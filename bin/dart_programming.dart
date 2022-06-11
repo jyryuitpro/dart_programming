8432812602218d1ee0fd61b04bb46cc136f38cd2
jyr <jyr@wavers.co.kr>
2022-06-11 20:12:06 +0900

    Dart #4 비동기 프로그래밍 (Async Programming) - 4

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -3,22 +3,29 @@ import 'dart:async';
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  final controller = StreamController();
-  final stream = controller.stream.asBroadcastStream();
+ // calculate(2).listen((val) {
+ //   print('calculate(2) : $val');
+ // });
 
-  // 짝수
-  final streamListener1 = stream.where((val) => val % 2 == 0).listen((val) {
-    print('Listener 1 : $val');
-  });
+ // calculate(4).listen((val) {
+ //   print('calculate(4) : $val');
+ // });
 
-  // 홀수
-  final streamListener2 = stream.where((val) => val % 2 == 1).listen((val) {
-    print('Listener 2 : $val');
-  });
+ playAllStream().listen((val) {
+   print(val);
+ });
+}
 
-  controller.sink.add(1);
-  controller.sink.add(2);
-  controller.sink.add(3);
-  controller.sink.add(4);
-  controller.sink.add(5);
+// Future의 await와 비슷한 기능
+Stream<int> playAllStream() async* {
+  yield* calculate(1);
+  yield* calculate(1000);
+}
+
+Stream<int> calculate(int number) async*{
+  for (int i = 0; i < 5; i++) {
+    yield i * number;
+
+    await Future.delayed(Duration(seconds: 1));
+  }
 }
