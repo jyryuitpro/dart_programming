cbfed87fcc9b49ea28617e6f6a558d311c1acdd4
jyr <jyr@wavers.co.kr>
2022-06-11 15:03:31 +0900

    Dart #2 객체지향 프로그래밍 (Object Oriented Programming) - 6

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,47 +1,25 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  BoyGroup bts = BoyGroup('BTS');
-  GirlGroup redVelvet = GirlGroup('레드벨벳');
-  // IdolInterface test = IdolInterface('블랙핑크');
+  List<String> names = [];
 
-  bts.sayName();
-  redVelvet.sayName();
+  Lecture<String, String> lecture1 = Lecture('123', 'lecture1');
 
-  print(bts is IdolInterface);
-  print(bts is BoyGroup);
-  print(bts is GirlGroup);
+  lecture1.printIdType();
 
-  print(redVelvet is IdolInterface);
-  print(redVelvet is BoyGroup);
-  print(redVelvet is GirlGroup);
+  Lecture<int, String> lecture2 = Lecture(123, 'lecture1');
+
+  lecture2.printIdType();
 }
 
-// interface (특수한 구조를 강제: 설계) | 인스턴스로 사용 불가
-abstract class IdolInterface {
-  String name;
+// generic - 타입을 외부에서 받을때 사용
+class Lecture<T, X> {
+  final T id;
+  final X name;
 
-  IdolInterface(this.name);
+  Lecture(this.id, this.name);
 
-  void sayName();
-}
-
-class BoyGroup implements IdolInterface {
-  String name;
-
-  BoyGroup(this.name);
-
-  void sayName() {
-    print('제 이름은 $name입니다.');
-  }
-}
-
-class GirlGroup implements IdolInterface {
-  String name;
-
-  GirlGroup(this.name);
-
-  void sayName() {
-    print('제 이름은 $name입니다.');
+  void printIdType() {
+    print(id.runtimeType);
   }
 }
