558e7ff61de34aa6b6f483f96c18e8187eb46c8a
jyr <jyr@wavers.co.kr>
2022-06-11 19:44:25 +0900

    Dart #4 비동기 프로그래밍 (Async Programming) - 2

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,14 +1,40 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
-void main() {
-  addNumbers(1, 1);
-  addNumbers(2, 2);
+void main() async {
+  // Future - 미래
+  // 미래에 받아올 값
+  Future<String> name = Future.value('코드팩토리');
+  Future<int> number = Future.value(1);
+  Future<bool> isTrue = Future.value(true);
+
+  print('함수 시작');
+
+  // 2개의 파라미터
+  // delayed - 지연되다.
+  // 1번 파라미터 - 지연할 기간 (얼마나 지연할건지) Duration
+  // 2번 파라미터 - 지연 시간이 지난 후 실행할 함수
+  Future.delayed(Duration(seconds: 2), () {
+    print('Delay 끝');
+  });
+
+  final result1 = await addNumbers(1, 1);
+  final result2 = await addNumbers(2, 2);
+
+  print('result1: $result1');
+  print('result2: $result2');
+  print('result1 + result2 = ${result1 + result2}');
 }
 
-// 1 + 1 = 2
-// 2 + 2 = 4
-void addNumbers(int number1, int number2) {
-  print('계산중: $number1 + $number2');
+Future<int> addNumbers(int number1, int number2) async {
+  print('계산 시작 : $number1 + $number2');
 
-  print('계산 완료: ${number1 + number2}');
+  // 서버 시뮬레이션
+  await Future.delayed(Duration(seconds: 2), () {
+    print('계산 완료 : $number1 + $number2 = ${number1 + number2}');
+  });
+
+  print('함수 완료 : $number1 + $number2');
+
+  // 자동으로 Future에 감싸서 함수가 리턴을 해준다.
+  return number1 + number2;
 }
