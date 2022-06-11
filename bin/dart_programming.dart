04bca0ad480b74caf4a14cf290b3812ee455e55d
jyr <jyr@wavers.co.kr>
2022-06-11 17:17:55 +0900

    Dart #3 함수형 프로그래밍 (Functional Programming) - 3

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,23 +1,14 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  Map<String, String> harryPotter = {
-    'Harry Potter': '해리포터',
-    'Ron Weasley': '론 위즐리',
-    'Hermione Granger': '헤르미온느 그레인저',
+  Set blackPinkSet = {
+    '로제',
+    '지수',
+    '제니',
+    '리사',
   };
 
-  final result = harryPotter.map((key, value) => MapEntry(
-        'Harry Potter Character $key',
-        '해리포터 캐릭터 $value',
-      ));
+  final newSet = blackPinkSet.map((x) => '블랙핑크 $x').toSet();
 
-  print(harryPotter);
-  print(result);
-
-  final keys = harryPotter.keys.map((x) => 'HPC $x').toList();
-  final values = harryPotter.keys.map((x) => '해리포터 $x').toList();
-
-  print(keys);
-  print(values);
+  print(newSet);
 }
