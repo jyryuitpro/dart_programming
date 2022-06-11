af7aa0d93d678d688a862cf2b1f79f9582620d19
jyr <jyr@wavers.co.kr>
2022-06-11 16:56:09 +0900

    Dart #3 함수형 프로그래밍 (Functional Programming) - 1

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,17 +1,39 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  Test test = Test();
+  List<String> blackPink = ['로제', '지수', '리사', '제니', '제니'];
 
-  // test.runtimeType
-  // test.hashCode
-  // test.toString()
-  // test.noSuchMethod(invocation)
-}
+  print(blackPink);
+  print(blackPink.asMap());
+  print(blackPink.toSet());
 
-// Object Oriented Programming
-// 객체지향 프로그래밍
-// class Test extends Object{
-class Test {
+  Map blackPinkMap = blackPink.asMap();
 
+  print(blackPinkMap.keys.toList());
+  print(blackPinkMap.values.toList());
+
+  Set blackPinkSet = Set.from(blackPink);
+
+  print(blackPinkSet.toList());
+
+  final newBlackPink = blackPink.map((x) {
+    return '블랙핑크 $x';
+  });
+
+  print(blackPink);
+  print(newBlackPink.toList());
+
+  final newBlackPink2 = blackPink.map((x) => '블랙핑크 $x');
+
+  print(newBlackPink2.toList());
+
+  print(blackPink == blackPink); // true
+  print(newBlackPink == blackPink); // false
+  print(newBlackPink == newBlackPink2); // false
+
+  String number = '13579';
+  
+  final parsed = number.split('').map((x) => '$x.jpg').toList();
+
+  print(parsed);
 }
