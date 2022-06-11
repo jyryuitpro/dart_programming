182362d7bff398192d84dfa0139530496e11f92e
jyr <jyr@wavers.co.kr>
2022-06-11 18:18:19 +0900

    Dart #3 함수형 프로그래밍 (Functional Programming) - 7

--- bin/dart_programming.dart
+++ bin/dart_programming.dart
@@ -1,20 +1,70 @@
 import 'package:dart_programming/dart_programming.dart' as dart_programming;
 
 void main() {
-  List<int> even = [
-    2, 4, 6, 8,
+  List<Map<String, String>> people = [
+    {
+      'name': '로제',
+      'group': '블랙핑크',
+    },
+    {
+      'name': '지수',
+      'group': '블랙핑크',
+    },
+    {
+      'name': 'RM',
+      'group': 'BTS',
+    },
+    {
+      'name': '뷔',
+      'group': 'BTS',
+    },
   ];
 
-  List<int> odd = [
-    1, 3, 5, 7,
-  ];
+  print(people);
 
-  // cascading operator
-  // ...
+  final parsedPeople = people
+      .map(
+        (x) => Person(
+          name: x['name']!,
+          group: x['group']!,
+        ),
+      )
+      .toList();
 
-  print([even, odd]);
-  print([...even, ...odd]);
-  print(even);
-  print([...even]);
-  print(even == [...even]);
+  print(parsedPeople);
+
+  for (Person person in parsedPeople) {
+    print(person.name);
+    print(person.group);
+  }
+
+  final bts = parsedPeople.where(
+    (x) => x.group == 'BTS',
+  );
+
+  print(bts);
+
+  final result = people.map(
+        (x) => Person(
+      name: x['name']!,
+      group: x['group']!,
+    ),
+  ).where((x) => x.group == 'BTS').fold<int>(0, (prev, next) => prev + next.name.length);
+
+  print(result);
+}
+
+class Person {
+  final String name;
+  final String group;
+
+  Person({
+    required this.name,
+    required this.group,
+  });
+
+  @override
+  String toString() {
+    return 'Person(name:$name, group:$group)';
+  }
 }
